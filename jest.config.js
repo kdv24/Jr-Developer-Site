620ca0e547e6cae5ae83f94432f41d97db29202a
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-03-12 08:00:57 -0700

    Create initial test configuration

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  globals: {
+    __PATH_PREFIX: '',
+  },
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.js',
+  },
+  setupFilesAfterEnv: [
+    'react-testing-library/cleanup-after-each',
+    'jest-dom/extend-expect',
+    '<rootDir>/test-utils/setup-tests.js',
+  ],
+  testPathIgnorePatterns: ['node_modules', '.cache'],
+  transform: {
+    '^.+\\.jsx?$': '<rootDir>/test-utils/jest-preprocess.js',
+  },
+  testURL: 'http://localhost',
+  // Ignore Gatsby as some modules are not transpiled
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+};
---
+++ test-utils/setup-tests.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+
+global.__loader = {
+  enqueue: jest.fn(),
+};
