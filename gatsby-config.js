9c75be213f74e9a2c548ca10c95ebeb9d5529cc9
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-03-19 23:00:12 -0700

    fix: add siteUrl for sitemap build

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    siteUrl: 'http://www.example.com',
   },
   plugins: [
     {
