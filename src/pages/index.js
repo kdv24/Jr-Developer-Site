3ccdf2c3633372766097c8f7c86009e85b8dab5e
Kelly de Vries <kdv@me.com>
2019-06-10 16:24:29 -0700

    add friendly message starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,14 @@ import Seo from '../components/seo';
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>
+      Hi people! {''}
+      <span role="img" aria-label="party">
+        🎉
+      </span>
+    </h1>
+    <p>Thanks for helping with our site!</p>
+    {/* <p>Let&#39;s build something great!</p> */}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
