88c6a4d736944332fdf429bf70627bed647eafa3
kevy7 <kvnyang503@gmail.com>
2019-07-01 21:49:26 -0700

    created the initial about us component and it's storybook

---
+++ src/components/aboutUs/aboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+
+const aboutUs = () => {
+    return (
+        <div className="aboutUs">
+            <p>This is the initial about us component</p>
+        </div>
+    )
+}
+
+export default aboutUs;
