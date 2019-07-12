4a4c323bdf40dbc53d14827a07f93c549b3877b8
kevy7 <kvnyang503@gmail.com>
2019-07-11 21:44:17 -0700

    removed aboutUs component from index component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import Image from '../components/image';
 import Seo from '../components/seo';
-import AboutUs from "../components/aboutUs/aboutUs";
 
 const IndexPage = () => (
   <Layout>
@@ -16,7 +15,6 @@ const IndexPage = () => (
       </span>
     </h1>
     <p>Thanks for helping with our site!</p>
-    <AboutUs />
     {/* <p>Let&#39;s build something great!</p> */}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
