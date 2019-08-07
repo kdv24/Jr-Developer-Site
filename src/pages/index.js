80a80ad1702d25c526c2b91b4a55ad0777ec7cb5
kevy7 <kvnyang503@gmail.com>
2019-08-06 21:14:02 -0700

    styled the volunteer component with css module

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 import AboutUs from '../components/aboutUs/aboutUs';
+import Volunteer from "../components/volunteer/volunteer";
 
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <AboutUs />
+    <Volunteer />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
