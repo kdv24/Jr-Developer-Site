8d4fa009483c0a4bb5a62262837ddb49a8dec5da
Kelly de Vries <kdv@me.com>
2019-08-07 16:51:57 -0700

    switch back to TwitterTimeLineEmbed component

--- src/components/twitter/twitter.js
+++ src/components/twitter/twitter.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import { TwitterFollowButton } from 'react-twitter-embed';
+import { TwitterFollowButton, TwitterTimelineEmbed } from 'react-twitter-embed';
 
 import './styles.css';
 
 const Twitter = () => (
-  <>
-    <div className="twitter__base">
-      <h3 className="twitter-title">Jr Dev Twitter Feed</h3>
-      <a
-        className="twitter-timeline"
-        href="https://twitter.com/pdx_jr_devs"
-        data-height="345"
-        data-min-width="10px !important"
-      >
-        {''}
-      </a>
-
-      <TwitterFollowButton screenName="pdx_jr_devs" />
-    </div>
-  </>
+  <div className="twitter__base">
+    <h3 className="twitter-title">Jr Dev Twitter Feed</h3>
+    <TwitterTimelineEmbed
+      sourceType="profile"
+      screenName="pdx_jr_devs"
+      options={{ height: 400 }}
+    />
+    <TwitterFollowButton screenName="pdx_jr_devs" />
+  </div>
 );
 
 export default Twitter;
