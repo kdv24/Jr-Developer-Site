7fa4d47ad7fc375a1052b09cea25d0358e7cfd4b
kevy7 <kvnyang503@gmail.com>
2019-08-06 21:14:02 -0700

    styled the volunteer component with css module

--- src/components/volunteer/volunteer.js
+++ src/components/volunteer/volunteer.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import styles from "./volunteer.module.css";
+
 class Volunteer extends Component {
 
     state = {
@@ -19,28 +21,35 @@ class Volunteer extends Component {
 
     render(){
         return (
-            <div className="volunteer">
+            <div className={styles.volunteerComponent}>
 
                 <form
+                    className={styles.volunteerForm}
                     name="volunteerForm"
                     method="POST"
                     action="https://formspree.io/kvnyang503@gmail.com"
                 >
-                    <label htmlFor="userName">Name: </label>
-                    <input type="text" name="name" id="userName" onChange={this.handleInputChanges} value={this.state.name} />
-                        
+                    <h2 className={styles.volunteerTitle}>Volunteer Form</h2>
+                    <div>
+                        <label htmlFor="userName">Name:</label>
+                        <input type="text" name="name" id="userName" onChange={this.handleInputChanges} value={this.state.name} />
+                    </div>
 
-                    <label htmlFor="email">Email:</label>
-                    <input type="text" name="email" id="email" onChange={this.handleInputChanges} value={this.state.email} />
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input type="text" name="email" id="email" onChange={this.handleInputChanges} value={this.state.email} />
+                    </div>
 
-                    <label htmlFor="volunteerList" >Volunteer Options: </label>
-                    <select name="volunteerList" id="volunteerList" value={this.state.volunteerList} onBlur={this.handleInputChanges} onChange={this.handleInputChanges}>
-                        <option value="twitterManagement" >Twitter Account Management</option>
-                        <option value="greeter" >Guess Greeter</option>
-                        <option value="signage" >Signage for Vacasa Nights</option>
-                        <option value="socialMediaManagement" >Social Media management</option>
-                    </select>
-                    <br />
+                    <div>
+                        <label htmlFor="volunteerList" >Volunteer Options: </label>
+                        <select name="volunteerList" id="volunteerList" value={this.state.volunteerList} onBlur={this.handleInputChanges} onChange={this.handleInputChanges}>
+                            <option value="twitterManagement" >Twitter Account Management</option>
+                            <option value="greeter" >Guess Greeter</option>
+                            <option value="signage" >Signage for Vacasa Nights</option>
+                            <option value="socialMediaManagement" >Social Media management</option>
+                        </select>
+                    </div>
+                    
                     
                     <button type="submit">Submit</button>
 
